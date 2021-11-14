d99675982dd810209f391afd6fee82b77380f218
ArmandoS98 <armandosantosec@gmail.com>
2021-11-14 13:10:17 -0600

    implementacion de las configraciones dentro del perfil adicionalmente a esto se agrego la funcionalidad de blue y correcciones

--- app/src/main/java/com/aesc/proyectofinaldesarrollomovil/ui/activities/AccountRecoveryActivity.kt
+++ app/src/main/java/com/aesc/proyectofinaldesarrollomovil/ui/activities/AccountRecoveryActivity.kt
@@ -26,10 +26,13 @@ class AccountRecoveryActivity : AppCompatActivity(), View.OnClickListener {
         val email = binding.emailEditText.text.toString()
         Firebase.auth.sendPasswordResetEmail(email).addOnCompleteListener { task ->
             if (task.isSuccessful) {
+                Toast.makeText(
+                    this, "Se ha enviado un correo de verifiación.",
+                    Toast.LENGTH_SHORT
+                ).show()
                 goToActivityF<LoginActivity>()
             } else {
-                Toast.makeText(this, "Ingrese un email de una cuenta valido", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(this, "Ingrese un email de una cuenta valido", Toast.LENGTH_SHORT).show()
             }
         }
     }
